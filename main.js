6cb50044bfc513e03d859dfdc6d8c8ace4e0e912
juli3p <julievicart13@gmail.com>
2019-04-18 10:45:47 +0200

    Exercices-3-JQuery

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('#arial').click(function() {
+    $('#text').css('font-family', 'arial');
+
+  })
+  $('#montserra').click(function(){
+    $('#text').css('font-family', 'montserra');
+  })
+
+})
+
+
+
+
+// $('#text').css('font-family', 'arial');
